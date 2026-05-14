426e8e0ccc4304487e74156a4a62a5c5f3a2590c
naomi-ho <151114635+naomi-ho@users.noreply.github.com>
2026-05-14 15:35:41 -0400

    add player constants, winning combinations, game state and cell event listeners

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+const PLAYER_X = 'X';
+const PLAYER_O = 'O';
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 const game = document.querySelector('.game');
+
+const gameState = {
+  boardArray: [],
+  currentPlayer: 'X',
+  gameActive: true,
+};
+
+function initGameState() {
+  gameState.boardArray = ['', '', '', '', '', '', '', '', ''];
+  gameState.currentPlayer = 'X';
+  gameState.gameActive = true;
+  console.log(gameState);
+}
 
 function createBoard() {
   const board = document.createElement('div');
@@ -8,10 +34,17 @@ function createBoard() {
     let cell = document.createElement('button');
     cell.className = 'cell';
     cell.dataset.index = i; // dataset.[] adds new data attribute, in this case 'index'
+    cell.addEventListener('click', handleMove);
     board.appendChild(cell);
   }
   game.appendChild(board);
 }
 
-// start game
-createBoard();
+function startGame() {
+  initGameState();
+  createBoard();
+}
+
+startGame();
+
+function handleMove() {}
